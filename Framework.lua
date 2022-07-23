3c11bb4a0d165353c377a08b3e96db326a66d03b
jkmmo <31679886+jkmmo@users.noreply.github.com>
2022-07-23 03:48:22 -0400

    Update Framework.lua

--- Framework.lua
+++ Framework.lua
@@ -1,6 +1,6 @@
 local BaseUrl = "https://raw.githubusercontent.com/jkmmo/ComputerCraftCode/main/"
 local Libs = {"AutoDoorLog"}
-
+print(Libs)
 for i,v in pairs(Libs) do
     print("Attempting to load"..v)
     local s,e = pcall(function ()
