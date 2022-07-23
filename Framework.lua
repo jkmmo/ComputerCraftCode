a26f9156d2a3fd41109a20b6b343d5b269646faa
jkmmo <31679886+jkmmo@users.noreply.github.com>
2022-07-23 03:41:06 -0400

    Update v1

--- Framework.lua
+++ Framework.lua
@@ -1,4 +1,5 @@
-local Urls = {"test","Test"}
+local BaseUrl = "https://raw.githubusercontent.com/jkmmo/ComputerCraftCode/main/"
+local Libs = {"AutoDoorLog"}
 
 for i,v in pairs(Urls) do
     print("i"..i,"v"..v)
