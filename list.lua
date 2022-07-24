53af97e8016d8b953206e9deb907441f8990addd
jkmmo <31679886+jkmmo@users.noreply.github.com>
2022-07-24 06:05:50 -0400

    Create list.lua

---
+++ list.lua
@@ -0,0 +1,13 @@
+local Chatbox = peripheral.find("chatBox")
+
+
+ip="147.135.44.154"
+
+str = http.get("https://mcapi.us/server/status?ip="..ip).readAll()
+obj = textutils.unserialiseJSON(str)
+players = obj.players.sample
+has_target = false
+
+for i,v in pairs(players) do
+    Chatbox.sendMessage(v.name,"Server")
+end
