c5571b34a3a6b59c926e89920c7168107c7e0c19
jkmmo <31679886+jkmmo@users.noreply.github.com>
2022-08-01 20:09:03 -0400

    Update BlockData2.lua

--- BlockData2.lua
+++ BlockData2.lua
@@ -1,18 +1,24 @@
 local Connected_peripherals = {}
-local printer = peripheral.find('printer')
+local printer = peripheral.find('printer') or error("A printer is needed for this program")
 
 for i,Side in pairs(peripheral.getNames()) do
     local per = peripheral.getType(Side)
     if per ~= 'printer' then
         local  name = per
         local p = peripheral.find(per)
-        local methods = peripheral.getMethods(per)
-        print(methods)
-        local tab = {name = name, p = p , methods = methods}
+        local tab = {name = name, p = p}
         table.insert(Connected_peripherals,tab)
     end
 end
 
 for i,pref in pairs(Connected_peripherals) do
-    print(pref.name,pref.methods)
+    local y = 1
+    printer.newPage()
+    printer.setPageTitle(pref.name)
+    for x,v in pairs(pref.p) do
+        printer.write(x..", "..v)
+        y = y + 1
+        printer.setCursorPos(1,y)
+    end
+    printer.endPage()
 end
